15c5f2da19d0b9560e4f155db7b9f32b4dc32f97
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-09 11:02:29 -0600

    play bar is working, there was a bug with the index, but I solve it

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,9 +1,11 @@
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import PlayPause from "./PlayPause";
-import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song,index, isPlaying, activeSong, data }) => {
+
+const SongCard = ({ song, i , isPlaying, activeSong, data }) => {
 
   const dispatch = useDispatch()
 
