3b783195a653d26a5de03261f706dd54e14898f1
Jose Maria Ureña Torres <chematorres@Joses-MacBook-Air.local>
2023-01-09 12:39:27 -0600

    working with the TOPPLAY Component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -1,5 +1,58 @@
-const TopPlay = () => (
-  <div>TopPlay</div>
-);
+import { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode } from "swiper";
+import PlayPause from "./PlayPause";
+import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+
+
+const TopChartCard = ({song, i})=>(
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer">
+    {song.title}
+  </div>
+)
+
+const TopPlay = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data } = useGetTopChartsQuery();
+  const divRef = useRef(null);
+  const topPlays = data?.slice(0, 5);
+
+  useEffect(()=>{
+    divRef.current.scrollIntoView({behavior: "smooth"})
+  });
+  
+  
+  const handlePauseClick = () => {
+    dispatch(playPause(false));};
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true)); };
+
+  return <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+    <div className="w-full flex flex-col">
+      <div className="flex flex-row justify-between items-center">
+        <h2 className="text-white font-bold text-2x1">Top Charts</h2>
+        <Link to="/top-charts">
+          <p className="text-gray-300 text-base">See more</p>
+        </Link>
+      </div>
+      <div className="mt-4 flex flex-col gap-1">
+        {topPlays?.map((song,i)=>(
+          <TopChartCard 
+          key={song.key}
+          song={song}
+          i={i}
+          
+          />
+        ))}
+      </div>
+    </div>
+  </div>;
+};
 
 export default TopPlay;
